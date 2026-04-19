7e36f18a8794a5d0811b954839e2513eb59e3bb4
cccq <geminicq5@outlook.com>
2026-04-19 18:45:12 +0800

    refactor(nvim): remove auto-lcd in favor of mini.nvim

--- plugin/90_neovide.lua
+++ plugin/90_neovide.lua
@@ -4,13 +4,4 @@ if vim.g.neovide then
   vim.g.neovide_refresh_rate = 60
   vim.g.neovide_confirm_quit = true
   vim.g.neovide_cursor_vfx_mode = "railgun"
-
-  vim.api.nvim_create_autocmd("BufEnter", {
-    callback = function()
-      local dir = vim.fn.expand("%:p:h")
-      if dir ~= "" and vim.fn.isdirectory(dir) == 1 then
-        vim.cmd("silent! lcd " .. vim.fn.fnameescape(dir))
-      end
-    end,
-  })
 end
