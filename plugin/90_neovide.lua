6af357247a3e117f725a67a6ebcf453199a9d83c
cccq <geminicq5@outlook.com>
2026-05-20 14:14:48 +0800

    feat(neovide): enable native window behavior and visual polish on macos only

--- plugin/90_neovide.lua
+++ plugin/90_neovide.lua
@@ -4,5 +4,11 @@ if vim.g.neovide then
   vim.g.neovide_refresh_rate = 60
   vim.g.neovide_confirm_quit = true
   vim.g.neovide_cursor_vfx_mode = "railgun"
-  vim.keymap.set("n", "<leader>k", "<Cmd>NeovideForceClick<CR>", { silent = true, desc = "*Click" })
+  if is_mac then
+    vim.g.neovide_window_blurred = true
+    vim.g.neovide_opacity = 0.8
+    vim.g.neovide_highlight_matching_pair = true
+    vim.g.neovide_proxy_icon = true
+    vim.keymap.set("n", "<leader>k", "<Cmd>NeovideForceClick<CR>", { silent = true, desc = "*Click" })
+  end
 end
